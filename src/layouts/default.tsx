8914af87fc0248e3da17560dd12c8cc5b5615f24
riiz08 <mhmdrizki0812@gmail.com>
2025-05-17 18:18:12 +0700

    add tele link

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -3,6 +3,7 @@ import Navbar from "@/components/navbar";
 import { ThemeSwitch } from "@/components/theme-switch";
 import { Button } from "@heroui/button";
 import { Image } from "@heroui/image";
+import { Link } from "@heroui/link";
 
 export default function DefaultLayout({
   children,
@@ -16,7 +17,14 @@ export default function DefaultLayout({
         {children}
       </main>
       <div className="fixed bottom-8 flex flex-col gap-2 right-8 z-30">
-        <Button isIconOnly variant="light" color="primary">
+        <Button
+          isIconOnly
+          variant="light"
+          color="primary"
+          as={Link}
+          isExternal={true}
+          href="https://t.me/+pBH5WCVyC0wxNWRl"
+        >
           <Image src="/logo/telegram.png" />
         </Button>
         <Button variant="light" color="primary" isIconOnly>
